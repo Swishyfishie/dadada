dfdd2cd32afed46cdaae8114c25e1da6e83d2dc2
Swishyfishie <pclandreiuk@gmail.com>
2020-09-25 00:37:46 +0100

    add getItems fetch request

--- backend/app/serializers/item_serializer.rb
+++ backend/app/serializers/item_serializer.rb
@@ -1,3 +1,3 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :calories
+  attributes :id, :name, :calories, :user_id
 end
--- frontend/index.js
+++ frontend/index.js
@@ -3,3 +3,18 @@ const BASE_URL = 'http://localhost:3000'
 document.addEventListener("load", () => {
     getItems()
 })
+
+function getItems(){
+    
+    let items = document.querySelector(".items")
+    items.innerHTML = ""
+    fetch(BASE_URL+"/items")
+    .then(res => res.json())
+    .then(items => {
+        items.innerHTML += items.map(item => {
+            `<li>
+            <a href="#" data-id="${item.id}">${item.name}</a>    
+            </li>`
+        })
+    })
+}
