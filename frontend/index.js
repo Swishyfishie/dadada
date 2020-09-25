56262607cca4ab324824edcace2a14aa74f9311e
Swishyfishie <pclandreiuk@gmail.com>
2020-09-25 01:01:40 +0100

    add item

--- frontend/index.js
+++ frontend/index.js
@@ -5,17 +5,37 @@ document.addEventListener("load", () => {
 })
 
 function getItems(){
-    
+    clearAll()
     let items = document.querySelector(".items")
     items.innerHTML = ""
     fetch(BASE_URL+"/items")
     .then(res => res.json())
     .then(i => {
-        console.log("")
         i.map(item => {
             items.innerHTML += `
                 <li data-id="${item.id}">${item.name} : ${item.calories}</li>
             `
         })
     })
+}
+
+function clearAll(){
+    document.querySelector(".items").innerHTML = ""
+}
+
+function addItem(){
+    let itemForm = document.querySelector(".item-form")
+
+    itemForm.innerHTML = `
+        <form>
+            <label for="name">
+            <input type="text">
+            <label for="calories">
+            <input type="number">
+            <input type="submit" value="Add Item" id="addItem">
+        </form>
+    `
+
+    let addItem = document.querySelector("#addItem")
+    addItem.addEventListener("click", postItem)
 }
